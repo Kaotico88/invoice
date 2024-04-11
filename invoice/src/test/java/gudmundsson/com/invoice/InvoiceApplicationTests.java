7ee824dfb8758c1537cfca2eedcbfd0bcada4bfb
Rene Gudmundsson <mikum777@gmail.com>
2024-04-11 16:33:13 -0400

    Se crea la primera version del proyecto Invoice

---
+++ invoice/src/test/java/gudmundsson/com/invoice/InvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gudmundsson.com.invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
