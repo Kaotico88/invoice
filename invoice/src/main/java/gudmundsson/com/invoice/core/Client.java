6e964f742297772684bf94878626b2ae891f203a
Rene Gudmundsson <mikum777@gmail.com>
2024-04-11 17:20:49 -0400

    Se empieza a conf la capa modelo o los cores

---
+++ invoice/src/main/java/gudmundsson/com/invoice/core/Client.java
@@ -0,0 +1,58 @@
+package gudmundsson.com.invoice.core;
+
+import java.sql.Date;
+
+public class Client {
+
+	private String clientId;
+	
+	private String customerType;
+	
+	private String idType;
+	
+	private Date activationDate;
+	
+	private Double totalDiscount;
+
+	public String getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+
+	public String getCustomerType() {
+		return customerType;
+	}
+
+	public void setCustomerType(String customerType) {
+		this.customerType = customerType;
+	}
+
+	public String getIdType() {
+		return idType;
+	}
+
+	public void setIdType(String idType) {
+		this.idType = idType;
+	}
+
+	public Date getActivationDate() {
+		return activationDate;
+	}
+
+	public void setActivationDate(Date activationDate) {
+		this.activationDate = activationDate;
+	}
+
+	public Double getTotalDiscount() {
+		return totalDiscount;
+	}
+
+	public void setTotalDiscount(Double totalDiscount) {
+		this.totalDiscount = totalDiscount;
+	}
+	
+	
+}
