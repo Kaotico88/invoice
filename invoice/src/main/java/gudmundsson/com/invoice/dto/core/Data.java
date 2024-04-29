08fb9f791bd0afb23516126aeb74247dd154784f
Rene Gudmundsson <mikum777@gmail.com>
2024-04-28 20:01:30 -0400

    Se mepieza con el trabajo del Service la entidad

--- invoice/src/main/java/gudmundsson/com/invoice/dto/core/Data.java
+++ invoice/src/main/java/gudmundsson/com/invoice/dto/core/Data.java
@@ -4,15 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import gudmundsson.com.invoice.core.Invoice;
+import gudmundsson.com.invoice.core.Service;
 
 public class Data {
 
 	private List<Invoice> invoices;
 	
+	private List<Service> services;
+	
 	public Data() {
 		this.invoices = new ArrayList<>();
+		this.services = new ArrayList<>();
 	}
-
+	
 	public List<Invoice> getInvoices() {
 		return invoices;
 	}
@@ -20,6 +24,15 @@ public class Data {
 	public void setInvoices(List<Invoice> invoices) {
 		this.invoices = invoices;
 	}
+
+	public List<Service> getServices() {
+		return services;
+	}
+
+	public void setServices(List<Service> services) {
+		this.services = services;
+	}
+	
 	
 	
 }
