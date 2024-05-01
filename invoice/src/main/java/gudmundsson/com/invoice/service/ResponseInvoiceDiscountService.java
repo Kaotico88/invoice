41ce4a564adeb3feda9ec4e70c2eabeb4e18ff52
Rene Gudmundsson <mikum777@gmail.com>
2024-04-30 22:49:44 -0400

    Se trabajo en que muestre los datos correctos con o sin descuentos

--- invoice/src/main/java/gudmundsson/com/invoice/service/ResponseInvoiceDiscountService.java
+++ invoice/src/main/java/gudmundsson/com/invoice/service/ResponseInvoiceDiscountService.java
@@ -19,90 +19,85 @@ public class ResponseInvoiceDiscountService {
 
 	@Autowired
 	private RQueryRepository rQueryRepository;
-	
+
 	public ResponseInvoiceDto getInvoiceDetails(String invoiceId) {
-		
+
 		Invoice invoice = rQueryRepository.getInvoiceById(Optional.of(invoiceId));
-		
+
 		Client client = rQueryRepository.getClientById(Optional.of(invoice.getClient().getClientId()));
-		
+
 		List<ItemService> itemServices = rQueryRepository.getServiceByClientId(Optional.of(client.getClientId()));
-		
+
 		double discountYears = getDiscoutYears(client, invoice);
-		
+
 		double discountServices = getDiscoutServices(itemServices, client);
-		
+
 		double totalNeto = discountYears + discountServices;
 		System.out.println("********Dentro el metodo GetinvoicesDetails************");
 		System.out.println("El descuento por años es:  " + discountYears);
 		System.out.println("El descuento por servicios es:  " + discountServices);
 		System.out.println("El total neto es:  " + totalNeto);
-		
-		ResponseInvoiceDto invoiceDto = new ResponseInvoiceDto(
-										invoice, itemServices, discountYears, discountServices, totalNeto);
-		
+
+		ResponseInvoiceDto invoiceDto = new ResponseInvoiceDto(invoice, itemServices, discountYears, discountServices,
+				totalNeto);
+
 		return invoiceDto;
 	}
 
 	private double getDiscoutYears(Client client, Invoice invoice) {
-		
+
 		double totalAmount;
-		double totalDiscount= 0;
-		
+		double totalDiscount = 0;
+
 		GregorianCalendar cale = new GregorianCalendar();
-        float currentYear = cale.get(Calendar.YEAR);
-        int currentDaysOfYear = Math.max(cale.get(Calendar.DAY_OF_YEAR), 365);
-        currentYear = currentYear + (currentDaysOfYear / 366);
-        
-        cale.setTime(client.getActivationDate());
-        double activationYear = cale.get(Calendar.YEAR);
-        int activatioDaysOfYear = Math.max(cale.get(Calendar.DAY_OF_YEAR), 365);
-        activationYear = activationYear + (activatioDaysOfYear / 366);       
-        double diffYear = currentYear - activationYear;
-        System.out.println("********Dentro el metodo Discont x Años************");
-        System.out.println("Los años despues de su activacion del cliente son:  " + diffYear);
-        if(diffYear >= 5) {
-        	double amountInvoice = invoice.getTotalAmount();
-        	System.out.println("Obteniedo el monto de invoice antes del descuento: " + amountInvoice);
-        	double discount = (5.0/100.0) * amountInvoice;
-        	totalAmount = amountInvoice - discount;
-        	invoice.setTotalAmount(totalAmount);
-        	System.out.println("Obteniedo el monto de invoice despues del descuento: " + amountInvoice);
-        	totalDiscount += 5;
-        	System.out.println("Obteniedo el monto de invoice despues del descuento: " + totalDiscount);
-        	client.setTotalDiscount(totalDiscount);
-        }else {
-        	totalAmount = invoice.getTotalAmount();
-        	totalDiscount += 0;
-        	client.setTotalDiscount(totalDiscount);
-        }
+		float currentYear = cale.get(Calendar.YEAR);
+		int currentDaysOfYear = Math.max(cale.get(Calendar.DAY_OF_YEAR), 365);
+		currentYear = currentYear + (currentDaysOfYear / 366);
+
+		cale.setTime(client.getActivationDate());
+		double activationYear = cale.get(Calendar.YEAR);
+		int activatioDaysOfYear = Math.max(cale.get(Calendar.DAY_OF_YEAR), 365);
+		activationYear = activationYear + (activatioDaysOfYear / 366);
+		double diffYear = currentYear - activationYear;
+		if (diffYear >= 5) {
+			double amountInvoice = invoice.getTotalAmount();
+			double discount = (5.0 / 100.0) * amountInvoice;
+			totalAmount = amountInvoice - discount;
+			invoice.setTotalAmount(totalAmount);
+			totalDiscount += 5;
+			client.setTotalDiscount(totalDiscount);
+		} else {
+			totalAmount = invoice.getTotalAmount();
+			totalDiscount += 0;
+			client.setTotalDiscount(totalDiscount);
+		}
 		return totalAmount;
 	}
-	
+
 	private double calculateTotalItemServiceAmount(List<ItemService> itemServices) {
-		
+
 		double totalItemServiceAmount = 0;
-		for(ItemService itemService : itemServices) {
+		for (ItemService itemService : itemServices) {
 			totalItemServiceAmount += itemService.getServiceAmount();
 		}
-		
 		return totalItemServiceAmount;
 	}
-	
+
 	private double getDiscoutServices(List<ItemService> itemServices, Client client) {
-		
+
 		double totalItemServiceAmount = calculateTotalItemServiceAmount(itemServices);
-		double totalAmont = 0;
+		double totalAmount = 0;
 		
-		if(itemServices.size() >= 2) {
-			double discount = (2.0 /100.0) * totalItemServiceAmount;
-			totalAmont = totalItemServiceAmount - discount;
+		if (itemServices.size() >= 2) {
+			double discount = (2.0 / 100.0) * totalItemServiceAmount;
+			totalAmount = totalItemServiceAmount - discount;
 			double totalDiscount = 2;
 			client.setTotalDiscount(totalDiscount);
-		}else {
+		} else {
+			totalAmount = totalItemServiceAmount;
 			client.setTotalDiscount(0.0);
 		}
-		return totalAmont;
+		return totalAmount;
 	}
-	
+
 }
